629de39ef73bd0b53c6c116146f8778c7c5f2133
matz2532 <49711043+matz2532@users.noreply.github.com>
2023-05-22 13:12:59 +0200

    removed not needed resultsFolder and MT image filename information

--- Images/InputData.py
+++ Images/InputData.py
@@ -7,73 +7,55 @@ def GetInputData():
     data["col-0"]["20170327 WT S1"] = {
     "labelledImageFilename": ['F_BOREALIS_MTs_LTi6b-GFP mChTUA5 WT 0h S1_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mChTUA5 WT 24h S1_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTIi6b-GFP mChTUA5 WT 48h S1_SMP-based (6) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTIi6b-GFP mChTUA5 WT 72h S1_SMP-based (6) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mChTUA5 WT 96h S1_SMP-based (6) MIP_WATERSHED_CLEAN.TIF'],
     "originalImageFilename": ['PM_LTi6b-GFP mChTUA5 WT 0h S1_SMP-based (3) MIP.TIF', 'PM_LTi6b-GFP mChTUA5 WT 24h S1_SMP-based (3) MIP.TIF', 'PM_LTIi6b-GFP mChTUA5 WT 48h S1_SMP-based (6) MIP.TIF', 'PM_LTIi6b-GFP mChTUA5 WT 72h S1_SMP-based (6) MIP.TIF', 'PM_LTi6b-GFP mChTUA5 WT 96h S1_SMP-based (6) MIP.TIF'],
-    "microtubuleImages": ['MTs_LTi6b-GFP mChTUA5 WT 0h S1_SMP-based (3) MIP.TIF', 'MTs_LTi6b-GFP mChTUA5 WT 24h S1_SMP-based (3) MIP.TIF', 'MTs_LTIi6b-GFP mChTUA5 WT 48h S1_SMP-based (6) MIP.TIF', 'MTs_LTIi6b-GFP mChTUA5 WT 72h S1_SMP-based (6) MIP.TIF', 'MTs_LTi6b-GFP mChTUA5 WT 96h S1_SMP-based (6) MIP.TIF'],
     "contourFilename": ['Cell_Contours_33_Cells -- LTi6b-GFP mChTUA5 WT 0h S1_.txt', 'Cell_Contours_33_Cells -- LTi6b-GFP mChTUA5 WT 24h S1_.txt', 'Cell_Contours_33 cells_0170327 WT S1  -- LTIi6b-GFP mChTUA5 WT 48h S1.txt', 'Cell_Contours_33_Cells -- LTIi6b-GFP mChTUA5 WT 72h S1_.txt', 'Cell_Contours_33_Cells -- LTi6b-GFP mChTUA5 WT 96h S1_.txt'],
-    "resultsFolder": "./results/UndulationDepth/20170327 WT S1/",
     "timePoints": ["0h", "24h", "48h", "72h", "96h"]
     }
     data["col-0"]["20170501 WT S1"] = {
     "labelledImageFilename": ['F_BOREALIS_MTs_20170501 WT 0h S1_SMP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20170501 WT 24h S1_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mChTUA5 48h S1_crop20-57_SMP-based (6) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mChTUA5 72h S1_SMP-based (6) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mChTUA5 96h S1_SMP-based (6) MIP_WATERSHED_CLEAN.TIF'],
     "originalImageFilename": ['PM_20170501 WT 0h S1_SMP.TIF', 'PM_20170501 WT 24h S1_SMP-based (3) MIP.TIF', 'PM_LTi6b-GFP mChTUA5 48h S1_crop20-57_SMP-based (6) MIP.TIF', 'PM_LTi6b-GFP mChTUA5 72h S1_SMP-based (6) MIP.TIF', 'PM_LTi6b-GFP mChTUA5 96h S1_SMP-based (6) MIP.TIF'],
-    "microtubuleImages": ['MTs_20170501 WT 0h S1_SMP.TIF', 'MTs_20170501 WT 24h S1_SMP-based (3) MIP.TIF', 'MTs_LTi6b-GFP mChTUA5 48h S1_crop20-57_SMP-based (6) MIP.TIF', 'MTs_LTi6b-GFP mChTUA5 72h S1_SMP-based (6) MIP.TIF', 'MTs_LTi6b-GFP mChTUA5 96h S1_SMP-based (6) MIP.TIF'],
     "contourFilename": ['Cell_Contours_27_Cells_20170501 WT 0h S1.txt', 'Cell_Contours_27_Cells_20170501 WT 24h S1.txt', 'Cell_Contours_27_Cells_LTi6b-GFP mChTUA5 48h S1.txt', 'Cell_Contours_27_Cells_LTi6b-GFP mChTUA5 72h S1.txt', 'Cell_Contours_27_Cells_LTi6b-GFP mChTUA5 96h S1.txt'],
-    "resultsFolder": "./results/UndulationDepth/20170501 WT S1/",
     "timePoints": ["0h", "24h", "48h", "72h", "96h"]
     }
     data["col-0"]["20170501 WT S2"] = {
     "labelledImageFilename": ['F_BOREALIS_MTs_LTi6b-GFP mChTUA5 0h S2_SMP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mChTUA5 24h S2_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mChTUA5 48h S2_SMP-based (5) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mChTUA5 72h S2_SMP-based (6) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mChTUA5 96h S2_SMP-based (6) MIP_WATERSHED_CLEAN.TIF'],
     "originalImageFilename": ['PM_LTi6b-GFP mChTUA5 0h S2_MIP.TIF', 'PM_LTi6b-GFP mChTUA5 24h S2_SMP-based (3) MIP.TIF', 'PM_LTi6b-GFP mChTUA5 48h S2_SMP-based (5) MIP.TIF', 'PM_LTi6b-GFP mChTUA5 72h S2_SMP-based (6) MIP.TIF', 'PM_LTi6b-GFP mChTUA5 96h S2_SMP-based (6) MIP.TIF'],
-    "microtubuleImages": ['MTs_LTi6b-GFP mChTUA5 0h S2_SMP.TIF', 'MTs_LTi6b-GFP mChTUA5 24h S2_SMP-based (3) MIP.TIF', 'MTs_LTi6b-GFP mChTUA5 48h S2_SMP-based (5) MIP.TIF', 'MTs_LTi6b-GFP mChTUA5 72h S2_SMP-based (6) MIP.TIF', 'MTs_LTi6b-GFP mChTUA5 96h S2_SMP-based (6) MIP.TIF'],
     "contourFilename": ['Cell_Contours_37_Cells_LTi6b-GFP mChTUA5 0h S2.txt', 'Cell_Contours_37_Cells_LTi6b-GFP mChTUA5 24h S2.txt', 'Cell_Contours_37_Cells_LTi6b-GFP mChTUA5 48h S2.txt', 'Cell_Contours_37_Cells_LTi6b-GFP mChTUA5 72h S2.txt', 'Cell_Contours_37_Cells_LTi6b-GFP mChTUA5 96h S2.txt'],
-    "resultsFolder": "./results/UndulationDepth/20170501 WT S2/",
     "timePoints": ["0h", "24h", "48h", "72h", "96h"]
     }
     data["Oryzalin"]["20170731 oryzalin S2"] = {
     "labelledImageFilename": ['F_BOREALIS_MTs_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 0h S2_MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 24h S2_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 48h S2_MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 72h S2_MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 96h S2_MIP_WATERSHED_CLEAN.TIF'],
     "originalImageFilename": ['PM_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 0h S2_SMP-based (3) MIP.TIF', 'PM_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 24h S2_SMP-based (3) MIP.TIF', 'PM_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 48h S2_MIP.TIF', 'PM_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 72h S2_MIP.TIF', 'PM_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 96h S2_MIP.TIF'],
-    "microtubuleImages": ['MTs_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 0h S2_SMP.TIF', 'MTs_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 24h S2_SMP-based (3) MIP.TIF', 'MTs_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 48h S2_MIP.TIF', 'MTs_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 72h S2_MIP.TIF', 'MTs_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 96h S2_MIP.TIF'],
     "contourFilename": ['Cell_Contours_26 cells_yzalin S2 - DO -- 20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 0h S2_.txt', 'Cell_Contours_26_Cells_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 24h S2.txt', 'Cell_Contours_26_Cells_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 48h S2.txt', 'Cell_Contours_26_Cells_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 72h S2.txt', 'Cell_Contours_26_Cells_20170731 LTi6b-GFP mCh-TUA5 1um oryzalin 96h S2.txt'],
-    "resultsFolder": "./results/UndulationDepth/20170731 oryzalin S2/",
     "timePoints": ["0h", "24h", "48h", "72h", "96h"]
     }
     data["Oryzalin"]["20171023 oryzalin S1"] = {
     "labelledImageFilename": ['F_BOREALIS_MTs_MAX_LTi6b-GFP mCh-TUA5 1um oryzalin 0h S1_MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mCh-TUA5 1um oryzalin 24h S1_MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mCh-TUA5 1um oryzalin 48h S1_MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mCh-TUA5 1um oryzalin 72h S1_MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_LTi6b-GFP mCh-TUA5 1um oryzalin 96h S1_SMP-based (9) MIP_WATERSHED_CLEAN.TIF'],
     "originalImageFilename": ['PM_MAX_LTi6b-GFP mCh-TUA5 1um oryzalin 0h S1_MIP.TIF', 'PM_LTi6b-GFP mCh-TUA5 1um oryzalin 24h S1_MIP.TIF', 'PM_LTi6b-GFP mCh-TUA5 1um oryzalin 48h S1_MIP.TIF', 'PM_LTi6b-GFP mCh-TUA5 1um oryzalin 72h S1_MIP.TIF', 'PM_LTi6b-GFP mCh-TUA5 1um oryzalin 96h S1_SMP-based (9) MIP.TIF'],
-    "microtubuleImages": ['MTs_MAX_LTi6b-GFP mCh-TUA5 1um oryzalin 0h S1_MIP.TIF', 'MTs_LTi6b-GFP mCh-TUA5 1um oryzalin 24h S1_MIP.TIF', 'MTs_LTi6b-GFP mCh-TUA5 1um oryzalin 48h S1_MIP.TIF', 'MTs_LTi6b-GFP mCh-TUA5 1um oryzalin 72h S1_MIP.TIF', 'MTs_LTi6b-GFP mCh-TUA5 1um oryzalin 96h S1_SMP-based (9) MIP.TIF'],
     "contourFilename": ['Cell_Contours_35_Cells_LTi6b-GFP mCh-TUA5 1um oryzalin 0h S1.txt', 'Cell_Contours_35_Cells_LTi6b-GFP mCh-TUA5 1um oryzalin 24h S1.txt', 'Cell_Contours_35_Cells_LTi6b-GFP mCh-TUA5 1um oryzalin 48h S1.txt', 'Cell_Contours_35_Cells_LTi6b-GFP mCh-TUA5 1um oryzalin 72h S1.txt', 'Cell_Contours_35_Cells_LTi6b-GFP mCh-TUA5 1um oryzalin 96h S1.txt'],
-    "resultsFolder": "./results/UndulationDepth/20171023 oryzalin S1/",
     "timePoints": ["0h", "24h", "48h", "72h", "96h"]
     }
     data["Oryzalin"]["20171106 oryzalin S1"] = {
     "labelledImageFilename": ['F_BOREALIS_MTs_20171106 oryzalin 0h S1_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20171106 oryzalin 24h S1_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20171106 oryzalin 48h S1_SMP-based (5) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20171106 oryzalin 72h S1_SMP-based (9) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20171106 oryzalin 96h S1_SMP-based (9) MIP_WATERSHED_CLEAN.TIF'],
     "originalImageFilename": ['PM_20171106 oryzalin 0h S1_SMP-based (3) MIP.TIF', 'PM_20171106 oryzalin 24h S1_SMP-based (3) MIP.TIF', 'PM_20171106 oryzalin 48h S1_SMP-based (5) MIP.TIF', 'PM_20171106 oryzalin 72h S1_SMP-based (9) MIP.TIF', 'PM_20171106 oryzalin 96h S1_SMP-based (9) MIP.TIF'],
-    "microtubuleImages": ['MTs_20171106 oryzalin 0h S1_SMP-based (3) MIP.TIF', 'MTs_20171106 oryzalin 24h S1_SMP-based (3) MIP.TIF', 'MTs_20171106 oryzalin 48h S1_SMP-based (5) MIP.TIF', 'MTs_20171106 oryzalin 72h S1_SMP-based (9) MIP.TIF', 'MTs_20171106 oryzalin 96h S1_SMP-based (9) MIP.TIF'],
     "contourFilename": ['Cell_Contours_23_Cells -- 20171106 oryzalin 0h S1_.txt', 'Cell_Contours_23_Cells -- 20171106 oryzalin 24h S1_.txt', 'Cell_Contours_23_Cells -- 20171106 oryzalin 48h S1_.txt', 'Cell_Contours_23_Cells -- 20171106 oryzalin 72h S1_.txt', 'Cell_Contours_23_Cells -- 20171106 oryzalin 96h S1_.txt'],
-    "resultsFolder": "./results/UndulationDepth/20171106 oryzalin S1/",
     "timePoints": ["0h", "24h", "48h", "72h", "96h"]
     }
     data["ktn1-2"]["20180618 ktn1-2 S1"] = {
     "labelledImageFilename": ['F_BOREALIS_MTs_20180618 ktn1-2 0h S1_MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20180618 ktn1-2 24h S1_SMP-based (3) MIP_WATERSHED_CLEAN.TIF','F_BOREALIS_MTs_20180618 ktn1-2 48h S1_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20180618 ktn1-2 72h S1_SMP-based (5) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20180618 ktn1-2 96h S1_crop_SMP-based (5) MIP_WATERSHED_CLEAN.TIF'],
     "originalImageFilename": ['PM_20180618 ktn1-2 0h S1_MIP.TIF', 'PM_20180618 ktn1-2 24h S1_SMP-based (3) MIP.TIF','PM_20180618 ktn1-2 48h S1_SMP-based (3) MIP.TIF', 'PM_20180618 ktn1-2 72h S1_SMP-based (3) MIP.TIF', 'PM_20180618 ktn1-2 96h S1_crop_SMP-based (5) MIP.TIF'],
-    "microtubuleImages": ['MTs_20180618 ktn1-2 0h S1_SMP.TIF', 'MTs_20180618 ktn1-2 24h S1_SMP-based (3) MIP.TIF','MTs_20180618 ktn1-2 48h S1_SMP-based (3) MIP.TIF', 'MTs_20180618 ktn1-2 72h S1_SMP-based (3) MIP.TIF', 'MTs_20180618 ktn1-2 96h S1_crop_SMP-based (5) MIP.TIF'],
     "contourFilename": ['Cell_Contours_35_Cells_20180618 ktn1-2 0h S1.txt', 'Cell_Contours_35_Cells_20180618 ktn1-2 24h S1.txt', 'Cell_Contours_35_Cells_20180618 ktn1-2 48h S1.txt', 'Cell_Contours_35_Cells_20180618 ktn1-2 72h S1.txt', 'Cell_Contours_35_Cells_20180618 ktn1-2 96h S1.txt'],
-    "resultsFolder": "./results/UndulationDepth/20180618 ktn1-2 S1/",
     "timePoints": ["0h", "24h", "48h", "72h", "96h"]
     }
     data["ktn1-2"]["20180618 ktn1-2 S2"] = {
     "labelledImageFilename": ['F_BOREALIS_MTs_20180618 ktn1-2 0h S2_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20180618 ktn1-2 24h S2_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20180618 ktn1-2 48h S2_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20180618 ktn1-2 72h S2 revised_SMP-based (5) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20180618 ktn1-2 96h S2_crop_SMP-based (3) MIP_WATERSHED_CLEAN.TIF'],
     "originalImageFilename": ['PM_20180618 ktn1-2 0h S2_SMP-based (3) MIP.TIF', 'PM_20180618 ktn1-2 24h S2_SMP-based (3) MIP.TIF', 'PM_20180618 ktn1-2 48h S2_SMP-based (3) MIP.TIF', 'PM_20180618 ktn1-2 72h S2 revised_MIP.TIF', 'PM_20180618 ktn1-2 96h S2_crop_SMP-based (3) MIP.TIF'],
-    "microtubuleImages": ['MTs_20180618 ktn1-2 0h S2_SMP-based (3) MIP.TIF', 'MTs_20180618 ktn1-2 24h S2_SMP-based (3) MIP.TIF', 'MTs_20180618 ktn1-2 48h S2_SMP-based (3) MIP.TIF', 'MTs_20180618 ktn1-2 72h S2 revised_SMP-based (5) MIP.TIF', 'MTs_20180618 ktn1-2 96h S2_crop_SMP-based (3) MIP.TIF'],
     "contourFilename": ['Cell_Contours_33_Cells -- 20180618 ktn1-2 0h S2_.txt', 'Cell_Contours_33_Cells -- 20180618 ktn1-2 24h S2_.txt', 'Cell_Contours_33_Cells -- 20180618 ktn1-2 48h S2_.txt', 'Cell_Contours_33_Cells -- 20180618 ktn1-2 72h S2 revised_.txt', 'Cell_Contours_33_Cells -- 20180618 ktn1-2 96h S2_.txt'],
-    "resultsFolder": "./results/UndulationDepth/20180618 ktn1-2 S2/",
     "timePoints": ["0h", "24h", "48h", "72h", "96h"]
     }
     data["ktn1-2"]["20180618 ktn1-2 S6"] = {
     "labelledImageFilename": ['F_BOREALIS_MTs_20180618 ktn1-2 0h S6_SMP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20180618 ktn1-2 24h S6_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20180618 ktn1-2 48h S6_SMP-based (3) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20180618 ktn1-2 72h S6_SMP-based (5) MIP_WATERSHED_CLEAN.TIF', 'F_BOREALIS_MTs_20180618 ktn1-2 96h S6_crop_SMP-based (5) MIP_WATERSHED_CLEAN.TIF'],
     "originalImageFilename": ['PM_20180618 ktn1-2 0h S6_SMP.TIF', 'PM_20180618 ktn1-2 24h S6_SMP-based (3) MIP.TIF', 'PM_20180618 ktn1-2 48h S6_SMP-based (3) MIP.TIF', 'PM_20180618 ktn1-2 72h S6_SMP-based (5) MIP.TIF', 'PM_20180618 ktn1-2 96h S6_crop_MIP.TIF'],
-    "microtubuleImages": ['MTs_20180618 ktn1-2 0h S6_SMP.TIF', 'MTs_20180618 ktn1-2 24h S6_SMP-based (3) MIP.TIF', 'MTs_20180618 ktn1-2 48h S6_SMP-based (3) MIP.TIF', 'MTs_20180618 ktn1-2 72h S6_SMP-based (5) MIP.TIF', 'MTs_20180618 ktn1-2 96h S6_crop_SMP-based (5) MIP.TIF'],
     "contourFilename": ['Cell_Contours_53_Cells -- 20180618 ktn1-2 0h S6_.txt', 'Cell_Contours_53_Cells -- 20180618 ktn1-2 24h S6_.txt', 'Cell_Contours_53_Cells -- 20180618 ktn1-2 48h S6_.txt', 'Cell_Contours_53_Cells_20180618 ktn1-2 72h S6.txt', 'Cell_Contours_53_Cells_20180618 ktn1-2 96h S6.txt'],
-    "resultsFolder": "./results/UndulationDepth/20180618 ktn1-2 S6/",
     "timePoints": ["0h", "24h", "48h", "72h", "96h"]
     }
     return data
