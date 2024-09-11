31e17e233d1554fbc5ab579c99f2bfebd82c8f46
matz2532 <49711043+matz2532@users.noreply.github.com>
2024-09-11 17:11:49 +0200

    adapted code to run through without error

--- Code/Analyze/EdgeRandomizationAnalysis.py
+++ Code/Analyze/EdgeRandomizationAnalysis.py
@@ -9,15 +9,15 @@ class EdgeRandomizationAnalysis:
 
     folderContents: MultiFolderContent = None
     currentSeed: int = 42
-    junctionPositionsOfContent: {"contentName": 1}
-    randomizationDifferencesPerContent: dict[str, list[list[float]]]
+    junctionPositionsOfContent: dict or None = None #dict[str, dict[int, list[list[float]]]] or None = None
+    randomizationDifferencesPerContent: dict or None = None # dict[str, list[list[float]]] or None
     # inner list of floats represents difference of original with randomization
     # outer list represents different entries from original
 
     def __init__(self, folderContentsFilename: str or MultiFolderContent):
         self.folderContents = MultiFolderContent(folderContentsFilename)
 
-    def SetJunctionPositionsOfContent(self, junctionPositionsKey: str, folderContentsFilename: str or MultiFolderContent or None):
+    def SetJunctionPositionsOfContent(self, junctionPositionsKey: str, folderContentsFilename: str or MultiFolderContent or None = None):
         if folderContentsFilename is not None:
             self.folderContents = MultiFolderContent(folderContentsFilename)
         assert self.folderContents is not None, f"The folder contents needs to be defined, when setting the junction positions of these contents."
@@ -25,16 +25,18 @@ class EdgeRandomizationAnalysis:
         self.junctionPositionsOfContent = {}
 
     def RandomizeEdgesWithoutPlanarityCheck(self,
-            randomizationSeed: int,
+            randomizationSeed: int or None = None,
             junctionPositionsKey: str or None = None,
             compareToValuesKey: str or None = None
         ):
-        if junctionPositionsKey is None:
-            assert self.junctionPositionsOfContent is not None, f"You need to either specify the junction positions of the corresponding contents (name being key) or specify the junctionPositionsKey parameter."
-        else:
+        if junctionPositionsKey is not None:
             self.SetJunctionPositionsOfContent(junctionPositionsKey=junctionPositionsKey)
             self.randomizationDifferencesPerContent = {}
-        self.currentSeed = randomizationSeed
+        assert self.junctionPositionsOfContent is not None, f"You need to either specify the junction positions of the corresponding contents (name being key) or specify the junctionPositionsKey parameter."
+        if randomizationSeed is None:
+            self.currentSeed += 1
+        else:
+            self.currentSeed = randomizationSeed
         # <----- implement from here
 
     def AnalyzeRandomizationResults(self, saveProperties: dict or None = None, showPlot: bool = False):
@@ -49,7 +51,8 @@ def testFunctionality():
     junctionPositionsKey = "finalJunctionFilename"
 
     filename = f"Images/{dataSetName}/{dataSetName}.json"
-    EdgeRandomizationAnalysis(filename)
+    randomizer = EdgeRandomizationAnalysis(filename)
+    randomizer.RandomizeEdgesWithoutPlanarityCheck(junctionPositionsKey=junctionPositionsKey)
 
 if __name__ == '__main__':
     testFunctionality()
