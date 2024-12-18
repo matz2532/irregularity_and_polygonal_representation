e5147a8c843beb9813192033e3f1a2ea7a6accdc
matz2532 <49711043+matz2532@users.noreply.github.com>
2024-12-18 16:35:02 +0100

    start creation of adjacency graph helper extracting adjacency graph from FolderContent and displaying graph with labelled image

---
+++ Code/MeasureCreator/AdjacencyGraphHelper.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import sys
+
+sys.path.insert(0, "./Code/DataStructures/")
+
+from FolderContent import FolderContent
+
+def calculateAdjacencyGraph(tissue: FolderContent, adjacencyListFilenameKey: str or None = None, neighborDistancesFilenameKey: str or None = None):
+    assert adjacencyListFilenameKey is not None or neighborDistancesFilenameKey is not None, f"You have to provide either the adjacencyListFilenameKey or the neighborDistancesFilenameKey for the calculation of the cellular adjacency graph of {tissue.GetTissueName()}"
+    if adjacencyListFilenameKey is not None:
+        adjacencyList = tissue.LoadKeyUsingFilenameDict(adjacencyListFilenameKey)
+        return nx.Graph(adjacencyList)
+    else:
+        neighborDistancesDf = tissue.LoadKeyUsingFilenameDict(neighborDistancesFilenameKey)
+        raise NotImplementedError("Extracting the adjacency graph using the neighborDistancesFilenameKey is not yet implemented.")
+
+def rotateImageToFitMatpltlibOrientation(image):
+    return np.rot90(np.flip(image, axis=0), k=3)
+
+def overlAyadjacencyGraphOnLabelledImage(tissue, displayInterestedCellsPositionFromLabelledImage: bool = False):
+    # from neighborDistances or labelledImageAdjacencyList -> extract adjacencyGraph
+    adjacencyGraph = calculateAdjacencyGraph(tissue, adjacencyListFilenameKey="labelledImageAdjacencyList")
+
+    labelledImage = tissue.LoadKeyUsingFilenameDict("labelledImageFilename")
+
+    pos = {}
+    for i in adjacencyGraph.nodes:
+        pixelPositionsOfNodes = np.where(labelledImage == i)
+        pos[i] = np.mean(pixelPositionsOfNodes, axis=1)
+    nx.draw_networkx(adjacencyGraph, pos=pos)
+
+    cellDict = tissue.LoadKeyUsingFilenameDict("cellContours") # enthält nur Zellen mit allen junctions known
+    posInnerCells = {}
+    sortedInterestedCells = np.sort(list(cellDict.keys()))
+    if displayInterestedCellsPositionFromLabelledImage:
+        # use labelled image to extract all positions -> know bordering cells from difference of cells
+        for i, cellId, in enumerate(sortedInterestedCells):
+            contour = cellDict[cellId]
+            posInnerCells[cellId] = np.mean(contour, axis=0)
+    else:
+        for innerCellId in sortedInterestedCells:
+            posInnerCells[innerCellId] = pos[innerCellId].tolist()
+
+    # remove non-interested cells
+    allCells = list(pos.keys())
+    cellsToRemove = np.setdiff1d(allCells, sortedInterestedCells)
+    adjacencyGraph.remove_nodes_from(cellsToRemove)
+    nx.draw_networkx_nodes(adjacencyGraph, posInnerCells, node_color="red")
+
+    # add labelled image
+    plt.imshow(rotateImageToFitMatpltlibOrientation(labelledImage))
+
+    plt.gca().legend(('ignored', 'adjacency', 'interested'))
+    plt.show()
+
+def main():
+    from MultiFolderContent import MultiFolderContent
+    dataSetname = "Eng2021Cotyledons"  # "Smit2023Cotyledons" #
+    filename = f"Images/{dataSetname}/{dataSetname}.json"
+    mfc = MultiFolderContent(filename)
+    tissueContent = list(mfc)[0]
+    overlAyadjacencyGraphOnLabelledImage(tissueContent)
+
+if __name__ == '__main__':
+    main()
